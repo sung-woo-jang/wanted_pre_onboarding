51e4954bf913a62bbaed6de8ca5f852e9736abb9
gyomdyung <seastory624@gmail.com>
2022-08-13 15:44:28 +0900

    :sparkles:Feature: User Entity & User모듈 등록

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,10 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+}
