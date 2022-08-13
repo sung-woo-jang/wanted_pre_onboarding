7f09c9284c24444dceacea71f1abf570dcb66fe7
gyomdyung <seastory624@gmail.com>
2022-08-13 16:04:27 +0900

    :sparkles:Feature: Board Entity Base

--- src/board/entities/board.entity.ts
+++ src/board/entities/board.entity.ts
@@ -6,5 +6,16 @@ export class Board extends BaseEntity {
   id: string;
 
   @Column({ type: 'varchar' })
-  name: string;
+  position: string;
+
+  @Column({ type: 'varchar' })
+  skill: string;
+
+  @Column({ type: 'int' })
+  reward: number;
+
+  @Column({ type: 'varchar' })
+  description: string;
 }
+
+// - 회사가 올린 다른 채용 공고
