cf708fe419a06cb78156eb8169374143773d57e1
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2022-08-14 21:48:23 +0900

    :sparkles:Feature: Swagger Api프로퍼티-Entities

--- src/board/entities/board.entity.ts
+++ src/board/entities/board.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Apply } from 'src/apply/entities/apply.entity';
 import { Company } from 'src/companies/entities/companies.entity';
@@ -12,31 +13,52 @@ import {
 
 @Entity()
 export class Board extends BaseEntity {
+  @ApiProperty({
+    example: '1',
+    description: '채용공고_id',
+  })
   @PrimaryGeneratedColumn()
-  @IsNumber()
   id: number;
 
+  @ApiProperty({
+    example: '백엔드 주니어',
+    description: '포지션',
+  })
   @Column({ type: 'varchar' })
   @IsNotEmpty()
   @IsString()
   position: string;
 
+  @ApiProperty({
+    example: 'NestJS',
+    description: '사용기술',
+  })
   @Column({ type: 'varchar' })
   @IsNotEmpty()
   @IsString()
   skill: string;
 
+  @ApiProperty({
+    example: 1000000,
+    description: '채용보상금',
+  })
   @Column({ type: 'int' })
   @IsNumber()
   reward: number;
 
+  @ApiProperty({
+    example: '원티드 랩에서 주니어 개발자를 채용합니다.',
+    description: '채용 내용',
+  })
   @Column({ type: 'varchar' })
   @IsString()
   description: string;
 
-  @ManyToOne((_) => Company, (company) => company.boards, { eager: false })
+  @ApiProperty({ type: () => Company })
+  @ManyToOne(() => Company, (company) => company.boards, { eager: false })
   company: Company;
 
+  @ApiProperty({ type: () => Apply })
   @OneToMany(() => Apply, (apply) => apply.board, { eager: true })
   apply: Apply[];
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as expressBasicAuth from 'express-basic-auth';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true })); // class-transformer가 적용되기 위한 옵션
+
+  app.use(
+    ['/docs', '/docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: { [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('wanted-pre-onboarding')
+    .setDescription('Wanted 프리 온보딩 API 설명서')
+    .setVersion('1.0.0')
+    .addTag('Wanted')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(5500);
 }
 bootstrap();
